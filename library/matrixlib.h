7a5f6c12a26572f914645647e246a4844588cb56
DekaR0n <margaritach_vl@mail.ru>
2022-11-07 21:58:12 +1000

    libraries

--- library/matrixlib.h
+++ library/matrixlib.h
@@ -10,22 +10,31 @@ typedef struct matrix {
     int **array;
 } matrix;
 
+// Создает новую матрицу и инициализирует ее размерами nxm.
 matrix *newMatrix(int size_n, int size_m);
 
+// Удаление матрицы.
 void destructor(matrix *Matrix);
 
+// Заолнение матрицы случайными числами в заданном диапазоне.
 matrix *fillRandom(matrix *Matrix, int d1, int d2);
 
+// Суммирование матриц.
 matrix *summarize(matrix *Matrix1, matrix *Matrix2);
 
+// Разность матриц.
 matrix *difference(matrix *Matrix1, matrix *Matrix2);
 
+// Произведение матриц.
 matrix *multiplication(matrix *Matrix1, matrix *Matrix2);
 
+// Транспонирование матрицы.
 matrix *transposition(matrix *Matrix);
 
+// Копирование матрицы.
 matrix *copy(matrix *Matrix, matrix *CopyMatrix);
 
+// Печать матрицы.
 void showMatrix(matrix *Matrix);
 
 #endif //__matrixlib__
